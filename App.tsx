6255a4dd43f32ef48a7ae6804fd7cb1c559ff83f
Furkan Cingöz <efcingoz@outlook.com>
2024-10-29 23:35:15 +0300

    start the basic styles

--- App.tsx
+++ App.tsx
@@ -4,18 +4,42 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>test!</Text>
+      <View style={styles.box}>
+        <Text style={styles.text}>1</Text>
+      </View>
+      <View style={styles.box}>
+        <Text style={styles.text}>2</Text>
+      </View>
+
+
+
+
       <StatusBar style="auto" />
     </View>
   );
 }
 
-//TODO: Add styles  here
+//TODO: Add styles  
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    gap: 10,
+  },
+  box: { 
+    backgroundColor: 'red',
+    width: 300,
+    height: 300,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 5,
+    borderColor: 'black',
+    borderRadius: 20,
+  },
+  text: {
+    color: 'yellow',
+    fontSize: 50,
   },
 });
