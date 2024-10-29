b5803c21b509e9610fb199dd4f5322a958d8c9f2
Furkan Cingöz <efcingoz@outlook.com>
2024-10-29 23:19:05 +0300

    test

--- App.tsx
+++ App.tsx
@@ -4,12 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>test!</Text>
       <StatusBar style="auto" />
     </View>
   );
 }
 
+//TODO: Add styles  here
 const styles = StyleSheet.create({
   container: {
     flex: 1,
